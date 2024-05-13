eea369f43de20ae6eccf2f9cbe75fe9b143461c6
nikhil_kumar <nkman2002@gmail.com>
2024-05-14 00:04:30 +0530

    conversion

--- polymorphism/conversion.cpp
+++ polymorphism/conversion.cpp
@@ -1,28 +1,62 @@
 #include<iostream>
 using namespace std;
 
+
+// primitive to class
+// class complax {
+//     private:
+//     int a ,b;
+//     public:
+
+
+
+//    complax(){
+    
+//    }
+//     complax(int x){
+//            a = x;
+//            b = 0;
+//     }
+//     void showdata(){
+//         cout<<a<<" "<<b;
+//     }
+// };
+
+// int main(){
+//     complax c1;
+//     int x=5;
+//     c1=x;
+//     c1.showdata();
+// }
+
+
+
+// class to primitive 
+
 class complax {
     private:
-    int a ,b;
+    int a;
+    int b;
+
     public:
 
-
-
-   complax(){
-    
-   }
-    complax(int x){
-           a = x;
-           b = 0;
+    void setdata(int x,int y){
+        a=x; b=y;
     }
+
     void showdata(){
-        cout<<a<<" "<<b;
+           cout<<a<<" "<<b<<endl;
+    }
+    operator int(){
+        return(a+b);
     }
 };
 
 int main(){
-    complax c1;
-    int x=5;
-    c1=x;
-    c1.showdata();
+complax c;
+c.setdata(3,5);
+c.showdata();
+int x;
+x=c;
+cout<<x;
 }
